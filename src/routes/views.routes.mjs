6f0ff0f5ffaae643770f3c994b54535ee47adcfe
alepaillas <alepaillas@gmail.com>
2024-06-08 12:56:56 -0400

    using tailwindcss

--- src/routes/views.routes.mjs
+++ src/routes/views.routes.mjs
@@ -2,8 +2,24 @@ import { Router } from "express";
 
 const router = Router();
 
+let adminUser = {
+  name: "ale",
+  last_name: "paillas",
+  role: "admin",
+};
+
+let testUser = {
+  name: "usuario",
+  last_name: "uno",
+  role: "user",
+};
+
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    style: "output.css",
+    user: adminUser,
+    isAdmin: adminUser.role === "admin", // va a guardar un booleano que usaremos para testear en la plantilla
+  });
 });
 
 export default router;
